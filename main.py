661e9c6bd9ed3b43f0257edebb4cf3cd2eba8e82
Q23RF <97279294+Q23RF@users.noreply.github.com>
2022-12-20 23:51:39 +0000

    BIG UPDATE: new db+query init+posting system deleted

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
 import os
 import requests
+import time
 from flask import Flask, session, abort, redirect, request, render_template
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from pip._vendor import cachecontrol
 import google.auth.transport.requests
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
 app = Flask('BooksRiver')
 app.secret_key = os.environ['SECRET_KEY']
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///info.db"
-db.init_app(app)
 
-
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String)
-
-
-class Post(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String)
-	title = db.Column(db.String)
-	subject = db.Column(db.String)
-	grade = db.Column(db.Integer)
-	status = db.Column(db.Integer)
-
-
-with app.app_context():
-	db.create_all()
-
+import sqlite3
+con = sqlite3.connect("instance/info.db", check_same_thread=False)
+cur = con.cursor()
+res = cur.execute("SELECT name FROM sqlite_master")
+print(res.fetchall())
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 client_secrets_file = "client_secret.json"
@@ -49,7 +33,7 @@ def login_is_required(function):
 
 	def wrapper(*args, **kwargs):
 		if "google_id" not in session:
-			return abort(401)  # Authorization required
+			return redirect("https://BooksRiver.q23rf.repl.co")  # Authorization required
 		else:
 			return function()
 
@@ -82,7 +66,7 @@ def callback():
 
 	session["google_id"] = id_info.get("sub")
 	session["name"] = id_info.get("name")
-	return redirect("/protected")
+	return redirect("/browse")
 
 
 @app.route("/logout")
@@ -102,23 +86,34 @@ def protected():
 	return render_template("protected.html", username=session['name'])
 
 
-@app.route("/post", endpoint='post')
+@app.route("/query", endpoint='query', methods=["GET", "POST"])
+@login_is_required
+def query():
+	if request.method == "POST":
+		target_subject = request.form["subject"]
+		q = cur.execute(f"SELECT * FROM books WHERE subject='{target_subject}'")
+		results = q.fetchall()
+		return render_template("query.html", results=results)
+	else:
+		return render_template("query.html")
+
+@app.route("/post", endpoint='post', methods=["GET", "POST"])
 @login_is_required
 def post():
-	return render_template("post.html")
+	if request.method == "POST":
+		return render_template("post.html", book=request.form["book"])
 
 @app.route("/browse", endpoint='browse', methods=["GET", "POST"])
 @login_is_required
 def browse():
 	if request.method == "POST":
-		post = Post(username=session['name'],
-		            title=request.form["title"],
-		            subject=request.form["subject"],
-		            grade=request.form["grade"],
-		            status=0)
-		db.session.add(post)
-		db.session.commit()
-	posts = db.session.execute(db.select(Post).order_by(Post.id)).scalars()
+		data = [request.form["book"],
+				(session['name'],
+				str(time.ctime()),
+		        0)]
+		cur.executemany("INSERT INTO posts VALUES(?, ?, ?, ?, ?)", data)
+		con.commit()
+
 	return render_template("browse.html", posts=posts)
 
 
