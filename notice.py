a96ea9a5209b248e4832efeee68e3d2255087d1f
Q23RF <97279294+Q23RF@users.noreply.github.com>
2022-12-29 10:15:36 +0000

    notice.py init

---
+++ notice.py
@@ -0,0 +1,14 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+def send_mail(to, subject, message):
+  password = "wiiphzjcybzxioxa"
+  mail = MIMEText(message, 'plain', 'utf-8')
+  mail['Subject'] = Header(subject, 'utf-8')
+
+  server = smtplib.SMTP('smtp.gmail.com')
+  server.starttls()
+  server.login("booksriver.noreply@gmail.com", password)
+  server.sendmail("booksriver.noreply@gmail.com", to, mail.as_string())
+  server.quit()
