d7d22fe84d58f8fad5afb6a9031aa3a423e75f6d
Q23RF <97279294+Q23RF@users.noreply.github.com>
2023-01-06 14:31:46 +0000

    privacy ploicy updated

--- main.py
+++ main.py
@@ -357,5 +357,9 @@ def censored():
 	con.commit()
 	return redirect("/review")
 
+@app.route("/policy", endpoint='policy')
+def policy():
+	return render_template("policy.html")
+
 if __name__ == '__main__':
 	app.run(port=8000, host='0.0.0.0', debug=False)
